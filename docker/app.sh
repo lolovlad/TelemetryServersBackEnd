9e58261b18833f2195ad3faf7d86a8b5e5240563
lolovlad <lolovlad33@gmail.com>
2024-04-01 14:51:21 +0400

    fix

--- docker/app.sh
+++ docker/app.sh
@@ -2,4 +2,4 @@
 
 alembic upgrade head
 python3 /app/create_model.py
-gunicorn MainServer.app:app --workers 2 --worker-class  uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+gunicorn src.app:app --workers 2 --worker-class  uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
